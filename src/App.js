13d30814665e4381553d1119b60846bdcfd3b081
KraiyosW <kraiyos.wanna@gmail.com>
2023-08-17 11:11:58 +0700

    feat: create dictionary webapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='flex flex-row'>
+        <img src="https://icons.iconarchive.com/icons/iconsmind/outline/128/Book-icon.png" width="64" height="64"/>
+      {/* <img src="https://icons.iconarchive.com/icons/iconsmind/outline/128/Play-Music-icon.png" width="128" height="128"/> */}
+        <h1></h1>
+      </div>
     </div>
   );
 }
